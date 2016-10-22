e9bc6e5fc1f940b41b44f1f0da2455cb7aea6446
Shubhang Malviya <shubhang.malviya@daffodilsw.com>
2016-10-22 17:58:20 +0530

    Delete, Post and Put request method creation.

--- libservices/src/main/java/com/libservices/delete/urlformencoded/FormUrlEncodedService.java
+++ libservices/src/main/java/com/libservices/delete/urlformencoded/FormUrlEncodedService.java
@@ -3,7 +3,6 @@ package com.libservices.delete.urlformencoded;
 import retrofit2.Call;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
 
 import java.util.Map;
 
@@ -12,7 +11,7 @@ public interface FormUrlEncodedService {
     String PATH_POST = "post";
 
     @FormUrlEncoded
-    @POST(PATH_POST)
+//    @HTTP
     Call<ResDeleteFormEncodedData> makeFormEncodedRequest(@FieldMap Map<String, String> requestMap);
 
 }
